7dfb0f1cbe806a07304af2b603e39c890722b5dc
pviktoria <pap.viktoria123@gmail.com>
2020-09-19 22:41:18 +0200

    poker

---
+++ poker.py
@@ -0,0 +1,26 @@
+
+import random
+
+v1= random.randrange(6)+1
+v2= random.randrange(6)+1
+v3= random.randrange(6)+1
+v4= random.randrange(6)+1
+v5= random.randrange(6)+1
+
+print("Gép " "%d" % v1, 	
+	"%d" % v2, 
+	"%d" % v3, 
+	"%d" % v4, 
+	"%d" % v5)
+	
+r1= random.randrange(6)+1
+r2= random.randrange(6)+1
+r3= random.randrange(6)+1
+r4= random.randrange(6)+1
+r5= random.randrange(6)+1
+
+print("Én " "%d" % r1, 	
+	"%d" % r2, 
+	"%d" % r3, 
+	"%d" % r4, 
+	"%d" % r5)
